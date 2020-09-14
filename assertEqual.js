3ef29e998e968d55f5aa00847244ffdd38cfc532
csjohn1 <csjohn@shaw.ca>
2020-09-14 20:44:44 +0000

    lotide assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("❤️ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("👽Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 12);
