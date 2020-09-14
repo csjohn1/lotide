aba7bbe2a9fd92283bc344dbbd6ea9665028b206
csjohn1 <csjohn@shaw.ca>
2020-09-14 21:01:03 +0000

    adding head.js

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`❤️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👽Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const head = function(array) {
+return array[0];
+
+}
+
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
