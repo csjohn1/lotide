88a0314ade487beb1474c60b5e8c406180fbd38f
csjohn1 <csjohn@shaw.ca>
2020-09-16 20:02:18 +0000

    adding countletters and countonly

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`❤️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👽Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+
+const countLetters = function(sentence) {
+  const result = {};
+  for (const letter of sentence) {
+    console.log(result, letter);
+    if (!result[letter]) {
+      result[letter] = 1; //giving a key and value
+     
+    } else {
+      result[letter] += 1;
+    }
+  }
+
+
+  return result;
+}
+
+console.log(countLetters('yaaasdfefaaa'));
