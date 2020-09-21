090245e58c6000e208369b09783508664925b24d
csjohn1 <csjohn@shaw.ca>
2020-09-21 17:33:26 +0000

    adding refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
 
-  if (actual === expected) {
-    console.log(`❤️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👽Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
 
 // const eqArrays = function(array1, array2) {
 
@@ -39,11 +31,4 @@ const eqArrays = function(array1, array2) {
 }
 
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
+module.exports = eqArrays;
