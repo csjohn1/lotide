0dd9f1e51a6318b4ec755c59017d50cf8c849365
csjohn1 <csjohn@shaw.ca>
2020-09-21 19:56:45 +0000

    adding headTest refactoring with mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,23 @@
 
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns undefined when comparing an empty array to 5", () => {
+    assert.isUndefined(head([]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+});
+
